55fc47415a259298b116d75d7a9cc7cdd3c6d5f4
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-26 00:09:39 +0300

    Add initial JavaScript files for gallery and form functionality

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,48 @@
-import { defineConfig } from 'vite';
-import { glob } from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import { defineConfig } from 'vite'; // Vite yapılandırma fonksiyonu
+import { glob } from 'glob'; // Dosya eşleştirme için Glob modülü
+import injectHTML from 'vite-plugin-html-inject'; // HTML eklemek için eklenti
+import FullReload from 'vite-plugin-full-reload'; // Tam sayfa yenileme için eklenti
+import SortCss from 'postcss-sort-media-queries'; // CSS medya sorgularını sıralamak için eklenti
 
 export default defineConfig(({ command }) => {
+  // Vite yapılandırmasını dışa aktar
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === 'serve' ? 'global' : '_global']: {}, // Komuta bağlı olarak global değişkenleri tanımla
     },
-    root: 'src',
+    root: 'src', // Kök dizini ayarla
     build: {
-      sourcemap: true,
+      sourcemap: true, // Kaynak haritalarını etkinleştir
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/*.html'), // Glob kullanarak HTML dosyalarını girdi olarak al
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
-              return 'vendor';
+              return 'vendor'; // node_modules için ayrı vendor parçası
             }
           },
           entryFileNames: chunkInfo => {
             if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+              return 'commonHelpers.js'; // commonHelpers parçası için özel isim
             }
-            return '[name].js';
+            return '[name].js'; // Varsayılan parça isimlendirme
           },
           assetFileNames: assetInfo => {
             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
+              return '[name].[ext]'; // HTML varlıkları için özel isimlendirme
             }
-            return 'assets/[name]-[hash][extname]';
+            return 'assets/[name]-[hash][extname]'; // Hash ile varsayılan varlık isimlendirme
           },
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      outDir: '../dist', // Çıkış dizini
+      emptyOutDir: true, // Çıkış dizinini boşalt
     },
     plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
+      injectHTML(), // HTML ekleme eklentisi
+      FullReload(['./src/**/**.html']), // Tam sayfa yenileme eklentisi
       SortCss({
-        sort: 'mobile-first',
+        sort: 'mobile-first', // Mobil öncelikli sıralama
       }),
     ],
   };
