859b1b07e8ee1deb2dc8ec3767ee71945eb10636
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-26 19:25:09 +0300

    Implement feedback form functionality and gallery layout

---
+++ src/js/01-gallery.js
@@ -0,0 +1,103 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryItems = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const gallery = document.querySelector('.js-gallery');
+
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.setAttribute('href', item.original);
+
+  galleryLink.addEventListener('click', event => {
+    event.preventDefault(); // Resim indirme işlemi durduruldu !!!!
+  });
+
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+    }
+  });
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.setAttribute('src', item.preview);
+  image.setAttribute('data-source', item.original);
+  image.setAttribute('alt', item.description);
+
+  galleryItem.appendChild(galleryLink);
+  galleryLink.appendChild(image);
+  gallery.appendChild(galleryItem);
+});
+
+var lightbox = new SimpleLightbox('.gallery li > a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/02.form.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', event => {
+  event.preventDefault();
+  console.log('Input event fired', event);
+  let email = form.elements.email.value;
+  let message = form.elements.message.value.toString().trim();
+  console.log('email:', email);
+  console.log('message:', message);
+
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email, message })
+  );
+});
+
+const { email, message } =
+  JSON.parse(localStorage.getItem('feedback-form-state')) || null;
+if (email) {
+  form.elements.email.value = email;
+}
+if (message) {
+  form.elements.message.value = message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let email = form.elements.email.value;
+  let message = form.elements.message.value.toString().trim();
+  console.log({
+    email,
+    message,
+  });
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+});
